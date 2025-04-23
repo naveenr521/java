950e146b075d578c022c4cafbf0e68e558732e01
Koppula Naveen Reddy <knaveenreddy21062000@gmail.com>
2025-04-23 16:48:32 +0530

    Data types

---
+++ java/Myjavaproject/src/Datatypes.java
@@ -0,0 +1,18 @@
+public class Datatypes {
+
+
+    public static void main(String args[]) {
+        String name = "Bro Code";
+        int gta = 6;
+        double pi = 3.14159;
+        char gender = 'M';
+        boolean isAdmin = true;
+
+
+        System.out.println(name);
+        System.out.println(gta);
+        System.out.println(pi);
+        System.out.println(gender);
+        System.out.println(isAdmin);
+    }
+}
