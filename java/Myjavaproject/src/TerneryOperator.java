67231b03730174d5a027e7b17a513bbcc01ab9b1
Koppula Naveen Reddy <knaveenreddy21062000@gmail.com>
2025-04-24 23:02:14 +0530

    Ternary operator

---
+++ java/Myjavaproject/src/TerneryOperator.java
@@ -0,0 +1,11 @@
+public class TerneryOperator {
+
+    public static void main(String args[]){
+
+        int score =86;
+
+        String PassOrFail=(score>=60) ? "PASS" :"FAIL";
+
+        System.out.println(PassOrFail);
+    }
+}
