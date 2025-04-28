685e035525a05756f37554093631458d7bc2c4d9
Koppula Naveen Reddy <knaveenreddy21062000@gmail.com>
2025-04-28 17:48:16 +0530

    Break and Continue statement example

---
+++ java/Myjavaproject/src/Breakstatement.java
@@ -0,0 +1,16 @@
+public class Breakstatement {
+
+
+    public static void main(String args[]){
+
+        for(int i =0;i<10;i++){
+
+            if(i==6){
+                continue;
+              //  break;
+            }
+            System.out.println(i);
+        }
+
+    }
+}
