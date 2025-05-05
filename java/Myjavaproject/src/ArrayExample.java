727ae04adf8920032616dd45837d155c142badf5
Koppula Naveen Reddy <knaveenreddy21062000@gmail.com>
2025-05-05 12:26:07 +0530

    Array Example

---
+++ java/Myjavaproject/src/ArrayExample.java
@@ -0,0 +1,13 @@
+public class ArrayExample {
+
+    public static void main(String args[]){
+
+        int[] arr={1,2,3,4,5};
+        int sum=0;
+        for(int element:arr){
+            sum+=element;
+
+        }
+        System.out.print(sum);
+    }
+}
