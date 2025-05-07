27332217d7e22b86a14ed273854095833ceef31a
Koppula Naveen Reddy <knaveenreddy21062000@gmail.com>
2025-05-07 22:47:50 +0530

    Constructor example

---
+++ java/Myjavaproject/src/Constructor.java
@@ -0,0 +1,17 @@
+class Student {
+    String name;
+    int age;
+
+    // Constructor
+    Student(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    void show() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
+
---
+++ java/Myjavaproject/src/Main2.java
@@ -0,0 +1,6 @@
+public class Main2 {
+    public static void main(String[] args) {
+        Student s1 = new Student("Alice", 20); // using constructor
+        s1.show(); // print details
+    }
+}
