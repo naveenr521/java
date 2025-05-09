f0041d78efd1d728349daae462d0291bd42697d3
Koppula Naveen Reddy <knaveenreddy21062000@gmail.com>
2025-05-09 18:06:52 +0530

    To String Example

---
+++ java/Myjavaproject/src/ExampleToString.java
@@ -0,0 +1,8 @@
+public class ExampleToString {
+
+    public static void main(String [] args){
+        Student1 s1= new Student1();
+
+        System.out.println(s1);
+    }
+}
---
+++ java/Myjavaproject/src/Student1.java
@@ -0,0 +1,9 @@
+public class Student1 {
+
+    int id =101;
+    String name="sai";
+
+    public String toString(){
+        return "student : " +name + ", Id : " + id;
+    }
+}
